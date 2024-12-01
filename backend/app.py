2a6361fe6175d9b94d39c6b36b5951efaadca10b
Stepan <s.ibatulov@yandex.ru>
2024-12-01 22:04:19 +0500

    simple i/o logic

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, url_for, request
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    error = None 
+    if request.method == 'POST':
+        print(request.args)
+        text = request.form['text']
+        processed_text = text.capitalize()
+        return response(processed_text)
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def index_post():
-    text = request.form['text']
-    processed_text = text.capitalize()
-    return welcome(processed_text)
-
-@app.route('/welcome')
-def welcome(name=None):
-    return render_template('welcome.html', name=name)
+@app.route('/response')
+def response(text=None):
+    return render_template('response.html', text=text)
 
 @app.route('/get', methods=['GET'])
 def index_get():
